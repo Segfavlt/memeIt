8969b0fa1ae0b8444e3aeac3b53c91b64728ea02
Jeff Schwanebeck <jschwanebeck@tuta.io>
2018-08-03 12:51:56 -0700

    updated gitignore, imported actions file in memeIt

--- memeIt/memeIt.py
+++ memeIt/memeIt.py
@@ -1,40 +1,16 @@
 #!/usr/bin/env python3
 import sys
 import argparse
+import actions
 
 
-class damnitAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string='-d'):
-        st  = "damnit " + values
-        st = st.upper()
-        print("```")
-        print(" ".join(st))
-        for (index,letter) in enumerate(st[1:],0):
-                # twice the index b/c of the characters in between
-                print('{0} {1}{0}'.format(letter,' '*2*index))
-        print("```")
-
-
-class spongeBobAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string='-s'):
-        letters = '' 
-        # add spaces to separate words
-        for value in values:
-            letters += value + ' '
-        meme = ''
-        for i in range(len(letters)):
-            if (i%2 != 0):
-                meme += letters[i].upper()
-            else:
-                meme += letters[i]
-        print(meme)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--damnit', nargs='?', action=damnitAction,
+    parser.add_argument('-d', '--damnit', nargs='?', action=actions.damnitAction,
             help='Prints the damnit box')
     parser.add_argument('-s', '--spongebob', nargs='*',
-            action=spongeBobAction, help='Prints the spongebob mocking meme')
+            action=actions.spongeBobAction, help='Prints the spongebob mocking meme')
     parser.parse_args()
 
     
