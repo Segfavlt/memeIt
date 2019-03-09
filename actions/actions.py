81449096764ff9816f3dd3d9de695f987d4c5ea1
Jeff Schwanebeck <jschwanebeck@tuta.io>
2019-03-09 13:18:16 -0800

    Fix table action
    Fixed missing + in value loop

--- actions/actions.py
+++ actions/actions.py
@@ -3,7 +3,7 @@ import memeIt
 
 class DamnitAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string='-d'):
-        st = "damnit " 
+        st = "damnit "
         for value in values:
             st += value
             st += " "
@@ -11,28 +11,28 @@ class DamnitAction(argparse.Action):
 
 class VorAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string='-V'):
-        print('''Look at them, they come to this place when they know they are 
-not pure. Tenno use the keys, but they are mere trespassers. Only I, 
-Vor, know the true power of the Void. I was cut in half, destroyed, 
-but through its Janus Key, the Void called to me. It brought me here 
-and here I was reborn. We cannot blame these creatures, they are 
-being led by a false prophet, an impostor who knows not the secrets 
-of the Void. Behold the Tenno, come to scavenge and desecrate this 
-sacred realm. My brothers, did I not tell of this day? Did I not  
-prophesize this moment? Now, I will stop them. Now I am changed, 
-reborn through the energy of the Janus Key. Forever bound to the 
-Void. Let it be known, if the Tenno want true salvation, they will 
-lay down their arms, and wait for the baptism of my Janus key. 
-It is time. I will teach these trespassers the redemptive power of 
-my Janus key. They will learn its simple truth. The Tenno are lost, 
-and they will resist. But I, Vor, will cleanse this place of their 
+        print('''Look at them, they come to this place when they know they are
+not pure. Tenno use the keys, but they are mere trespassers. Only I,
+Vor, know the true power of the Void. I was cut in half, destroyed,
+but through its Janus Key, the Void called to me. It brought me here
+and here I was reborn. We cannot blame these creatures, they are
+being led by a false prophet, an impostor who knows not the secrets
+of the Void. Behold the Tenno, come to scavenge and desecrate this
+sacred realm. My brothers, did I not tell of this day? Did I not
+prophesize this moment? Now, I will stop them. Now I am changed,
+reborn through the energy of the Janus Key. Forever bound to the
+Void. Let it be known, if the Tenno want true salvation, they will
+lay down their arms, and wait for the baptism of my Janus key.
+It is time. I will teach these trespassers the redemptive power of
+my Janus key. They will learn its simple truth. The Tenno are lost,
+and they will resist. But I, Vor, will cleanse this place of their
 impurity.''')
 
 
 
 class BoxAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string='-b'):
-        st = "damnit " 
+        st = "damnit "
         for value in values:
             st += value
             st += " "
@@ -47,9 +47,10 @@ class BoxAction(argparse.Action):
 class TableAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string='-t'):
         st = ""
+        print(values)
         for value in values:
             st += value
-            st = " "
+            st += " "
         print("```")
         print(" ".join(st))
         for (index,letter) in enumerate(st[1:],0):
@@ -59,7 +60,7 @@ class TableAction(argparse.Action):
 
 class SpongeBobAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string='-s'):
-        letters = '' 
+        letters = ''
         # add spaces to separate words
         for value in values:
             letters += value + ' '
